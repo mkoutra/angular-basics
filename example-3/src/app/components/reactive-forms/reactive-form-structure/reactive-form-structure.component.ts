fc3d934c8cd43bc4b03e3e731b5f86ffdc0ecdc3
mkoutra <koutra98@hotmail.com>
2024-10-22 19:28:19 +0300

    Reactive form with angular material

--- example-3/src/app/components/reactive-forms/reactive-form-structure/reactive-form-structure.component.ts
+++ example-3/src/app/components/reactive-forms/reactive-form-structure/reactive-form-structure.component.ts
@@ -2,10 +2,23 @@ import { Component } from '@angular/core';
 
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
+// Following the angular material documentation
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-reactive-form-structure',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    // Material from here and below
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
+    ],
   templateUrl: './reactive-form-structure.component.html',
   styleUrl: './reactive-form-structure.component.css'
 })
@@ -14,7 +27,7 @@ export class ReactiveFormStructureComponent {
     userForm = new FormGroup({
         givenName: new FormControl('', Validators.required), // Initial value is empty and the value is mandatory
         surName: new FormControl('', Validators.required),
-        age: new FormControl("18", [
+        age: new FormControl(18, [  // I'm not sure if it should be 18 or '18'
             Validators.required,
             Validators.pattern('^[0-9]*$'),
             Validators.min(18),
