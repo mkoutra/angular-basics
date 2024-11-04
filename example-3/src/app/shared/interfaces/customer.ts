d1dcf906381f119ccd84f0cf5ecb0a524dec1776
mkoutra <koutra98@hotmail.com>
2024-11-04 20:48:07 +0200

    Create user form created

---
+++ example-3/src/app/shared/interfaces/customer.ts
@@ -0,0 +1,19 @@
+export interface PhoneNumber {
+    number: string;
+    type: string;
+}
+
+export interface Address {
+    street: string;
+    city: string;
+    zipcode: string;
+}
+
+export interface Customer {
+    givenName: string;
+    surName: string;
+    email: string;
+    afm: string;
+    phoneNumbers: PhoneNumber[];
+    address: Address;
+}
---
+++ example-3/src/app/shared/services/customer.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, inject } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';  // To do http calls
+import { environment } from '../../../environments/environment.development';
+import { Customer } from '../interfaces/customer';
+
+const API_URL = `${environment.apiURL}/customer`
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  http: HttpClient = inject(HttpClient);
+
+  createCustomer(customer: Customer) {
+    return this.http.post<{msg: string}>(`${API_URL}/create`, customer);
+  }
+}
